ba2c1b59e1ae58d5538bbbadbd94eb920a5548e5
ODINY <60807076+ODINY@users.noreply.github.com>
2020-03-09 17:44:11 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage.mk
+    $(LOCAL_DIR)/lineage_m253.mk
+    
+COMMON_LUNCH_CHOICES := \
+    lineage_m253-user \
+    lineage_m253-userdebug \
+    lineage_m253-eng
